2da24ecb10816ebc7c817429c6a6b6c46fe5ed07
Benjarke <mr.ben.clarke@gmail.com>
2015-04-13 10:36:41 +0100

    Adding All Files

---
+++ app/src/main/java/com/prey/activities/WelcomeActivity.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Created by Carlos Yaconi
+ * Copyright 2012 Fork Ltd. All rights reserved.
+ * License: GPLv3
+ * Full license at "/LICENSE"
+ ******************************************************************************/
+package com.prey.activities;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.prey.R;
+public class WelcomeActivity extends PreyActivity {
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// ignore orientation change
+		super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		try{
+			Button newUser = (Button) findViewById(R.id.btn_welcome_newuser);
+		
+		
+			newUser.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(WelcomeActivity.this, CreateAccountActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+		}catch(Exception e){}
+	}
+
+}
